dd6fd5f0688b0523cc57121a61d7cc157cbc6700
Thunder <thunder@disroot.org>
2024-03-27 21:29:23 +0200

    Stupid way to install aajanki's yleareena kodi plugin

--- build.sh
+++ build.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 
-which nixos-rebuild 1> /dev/null 2> /dev/null && {
-    sudo nixos-rebuild switch --flake path:#default --show-trace;
-} || {
+nixos() {
+    sudo nixos-rebuild switch --flake path:#default --show-trace
+    exit 0 # Otherwise home will run on failure
+}
+
+home() {
     nix run path: -- switch --flake path: --show-trace
 }
 
+which nixos-rebuild 1> /dev/null 2> /dev/null && nixos || home
--- home/tv/default.nix
+++ home/tv/default.nix
@@ -1,14 +1,68 @@
-{ config, lib, pkgs, localconfig, ... }: {
+{
+  config,
+  lib,
+  pkgs,
+  localconfig,
+  inputs,
+  ...
+}: let
+  addonDir = "/share/kodi/addons";
+  yleNamespace = "plugin.video.yleareena.jade";
+
+  yleareena = pkgs.stdenv.mkDerivation rec {
+    name = "kodi-yleareena-1.3.1";
+    dontStrip = true;
+
+    extraRuntimeDependencies = [];
+    propagatedBuildInputs = [];
+
+    src = pkgs.fetchgit {
+      url = "https://github.com/aajanki/${yleNamespace}";
+      rev = "9b89fabf6ad1cae08d92b3309029061ca9ab66e5";
+      hash = "sha256-uLWWJTX6aHsjzJneqab2VczJO+33tg8nP7fPQ/GpqZg=";
+    };
+
+    passthru = {
+      pythonPath = "resources/lib";
+    };
+
+    installPhase = ''
+    runHook preInstall
+
+    cd ./$sourceDir
+    d=$out${addonDir}/${yleNamespace}
+    mkdir -p $d
+    sauce="."
+    [ -d ${yleNamespace} ] && sauce=${yleNamespace}
+    cp -R "$sauce/"* $d
+
+    runHook postInstall
+    '';
+    
+  };
+in {
   config = lib.mkIf (localconfig.install.tv) (with config; {
+    home.packages = [
+      yleareena
+    ];
+
+    xdg.dataFile."kodi/addons/${yleNamespace}" = {
+      enable = true;
+      recursive = true;
+      source = "${yleareena}/share/kodi/addons/${yleNamespace}";
+    };
+
     programs.kodi = {
       enable = true;
       datadir = "${xdg.dataHome}/kodi";
-      package = pkgs.kodi.withPackages (exts: with exts; [
-        youtube
-        netflix
-        jellyfin
-        invidious
-      ]);
+
+      package = pkgs.kodi.withPackages (exts:
+        with exts; [
+          youtube
+          netflix
+          jellyfin
+          invidious
+        ]);
     };
   });
 }
