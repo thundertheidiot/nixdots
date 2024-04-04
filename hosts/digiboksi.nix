a67fd4b2dd63c351d4325aa9f597d81fcdc26e0f
Thunder <thunder@disroot.org>
2024-04-04 22:28:03 +0300

    digiboksi host

---
+++ hosts/digiboksi.nix
@@ -0,0 +1,76 @@
+{
+  systemArch = "x86_64-linux";
+  username = "tv";
+  homeDirectory = "/home/tv";
+
+  options = {
+    config,
+    pkgs,
+    ...
+  }: {
+    config = {
+      username = "tv";
+      hostName = "digiboksi";
+      timeZone = "Europe/Helsinki";
+
+      setup.userMachine.enable = true;
+      setup.hyprland.enable = true;
+      setup.hyprland.extraConfig = "";
+      setup.hyprland.extraAutostart = [];
+      setup.awesomeWM.enable = false;
+      setup.firefox.enable = true;
+      setup.gaming.enable = false;
+      setup.tv.enable = true;
+
+      setup.gpu = "intel";
+    };
+  };
+
+  home = {...}: {
+    home.stateVersion = "24.05";
+  };
+
+  system = {
+    lib,
+    pkgs,
+    config,
+    modulesPath,
+    ...
+  }: {
+    # Machine specific configuration, filesystems, bootloader, basically hardware-configuration.nix + system.stateVersion
+    boot.loader.grub = {
+      enable = true;
+      device = "/dev/sda";
+      extraConfig = ''
+        set timeout=1
+      '';
+    };
+
+    hardware.opengl.enable = true;
+
+    system.stateVersion = "23.11";
+
+    boot.initrd.availableKernelModules = ["xhci_pci" "ehci_pci" "ahci" "nvme" "usbhid" "usb_storage" "sd_mod"];
+    boot.initrd.kernelModules = [];
+    boot.kernelModules = ["kvm-intel"];
+    boot.extraModulePackages = [];
+
+    fileSystems."/" = {
+      device = "/dev/disk/by-uuid/721918a3-0d55-4b0b-b531-484b224568a1";
+      fsType = "btrfs";
+      options = ["subvol=@"];
+    };
+
+    fileSystems."/boot" = {
+      device = "/dev/disk/by-uuid/157d9ce8-9d0f-408f-b489-2478a285ef7b";
+      fsType = "ext4";
+    };
+
+    swapDevices = [];
+
+    networking.useDHCP = lib.mkDefault true;
+
+    nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+    hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+  };
+}
