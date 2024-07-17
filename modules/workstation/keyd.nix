8e25440f7bb160b4c161c71634063f2ed4c21b09
Thunder <thunder@disroot.org>
2024-07-17 12:58:18 +0300

    nix fmt

--- modules/workstation/keyd.nix
+++ modules/workstation/keyd.nix
@@ -1,77 +1,83 @@
 {
-  system = { lib, config, pkgs, ... }: lib.mkIf config.workstation.keyd.enable {
-    services.keyd = {
-      enable = true;
+  system = {
+    lib,
+    config,
+    pkgs,
+    ...
+  }:
+    lib.mkIf config.workstation.keyd.enable {
+      services.keyd = {
+        enable = true;
 
-      keyboards.default = {
-        ids = config.workstation.keyd.ids;
+        keyboards.default = {
+          ids = config.workstation.keyd.ids;
 
-        settings = {
-          main = {
-            capslock = "overload(meta, esc)";
+          settings = {
+            main = {
+              capslock = "overload(meta, esc)";
 
-            space = "overload(numpad, space)";
+              space = "overload(numpad, space)";
 
-            # hrm
-            # a = "timeout(a, 200, layer(meta))";
-            a = "overload(meta, a)";
-            s = "overload(alt, s)";
-            d = "overload(shift, d)";
-            f = "overload(control, f)";
+              # hrm
+              # a = "timeout(a, 200, layer(meta))";
+              a = "overload(meta, a)";
+              s = "overload(alt, s)";
+              d = "overload(shift, d)";
+              f = "overload(control, f)";
 
-            j = "overload(control, j)";
-            k = "overload(shift, k)";
-            l = "overload(meta, l)";
-            ";" = "overload(meta, ;)";
-          };
+              j = "overload(control, j)";
+              k = "overload(shift, k)";
+              l = "overload(meta, l)";
+              ";" = "overload(meta, ;)";
+            };
 
-          numpad = {
-            m = "1";
-            "," = "2";
-            "." = "3";
-            j = "4";
-            k = "5";
-            l = "6";
-            u = "7";
-            i = "8";
-            o = "9";
-            rightalt = "0";
+            numpad = {
+              m = "1";
+              "," = "2";
+              "." = "3";
+              j = "4";
+              k = "5";
+              l = "6";
+              u = "7";
+              i = "8";
+              o = "9";
+              rightalt = "0";
 
-            a = "layer(meta)";
-            s = "layer(alt)";
-            d = "layer(shift)";
-            f = "layer(control)";
+              a = "layer(meta)";
+              s = "layer(alt)";
+              d = "layer(shift)";
+              f = "layer(control)";
 
-            g = "toggle(normal)";
-          };
+              g = "toggle(normal)";
+            };
 
-          normal = {
-            capslock = "overload(meta, esc)";
-            rightalt = "toggle(normal)";
+            normal = {
+              capslock = "overload(meta, esc)";
+              rightalt = "toggle(normal)";
 
-            a = "a";
-            s = "s";
-            d = "d";
-            f = "f";
+              a = "a";
+              s = "s";
+              d = "d";
+              f = "f";
 
-            j = "j";
-            k = "k";
-            l = "l";
-            ";" = ";";
+              j = "j";
+              k = "k";
+              l = "l";
+              ";" = ";";
 
-            space = "space";
-          };
+              space = "space";
+            };
 
-          shift = {
-            esc = "~";
-          };
-          meta = {
-            esc = "`";
+            shift = {
+              esc = "~";
+            };
+            meta = {
+              esc = "`";
+            };
           };
         };
       };
     };
-  };
 
   home = {...}: {};
 }
