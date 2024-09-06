b69a993a362e0216b4ee0734dc21c24d75be1298
Thunder <thunder@disroot.org>
2024-09-06 15:02:18 +0300

    yea

---
+++ modules/programs/discord/plugins.nix
@@ -0,0 +1,182 @@
+{
+  ChatInputButtonAPI.enabled = true;
+  CommandsAPI.enabled = true;
+  MemberListDecoratorsAPI.enabled = true;
+  MessageAccessoriesAPI.enabled = true;
+  MessageDecorationsAPI.enabled = true;
+  MessageEventsAPI.enabled = true;
+  MessageUpdaterAPI.enabled = true;
+  UserSettingsAPI.enabled = true;
+  WebContextMenus = {
+    addBack = true;
+    enabled = true;
+  };
+  Settings = {
+    enabled = true;
+    settingsLocation = "aboveNitro";
+  };
+  NoTrack = {
+    disableAnalytics = true;
+    enabled = true;
+  };
+
+  AlwaysAnimate.enabled = true;
+  AlwaysTrust = {
+    domain = true;
+    enabled = true;
+    file = true;
+  };
+  AnonymiseFileNames = {
+    anonymiseByDefault = true;
+    consistent = "image";
+    enabled = true;
+    method = 0;
+    randomisedLength = 7;
+  };
+  BetterSettings = {
+    disableFade = true;
+    eagerLoad = true;
+    enabled = true;
+    organizeMenu = true;
+  };
+  BiggerStreamPreview.enabled = true;
+  BlurNSFW = {
+    blurAmount = 10;
+    enabled = true;
+  };
+  CallTimer = {
+    enabled = true;
+    format = "stopwatch";
+  };
+  Decor = {
+    agreedToGuidelines = false;
+    enabled = true;
+  };
+  FakeNitro = {
+    disableEmbedPermissionCheck = false;
+    emojiSize = 48;
+    enableEmojiBypass = true;
+    enableStickerBypass = true;
+    enableStreamQualityBypass = false;
+    enabled = true;
+    hyperLinkText = "{{NAME}}";
+    stickerSize = 160;
+    transformCompoundSentence = true;
+    transformEmojis = true;
+    transformStickers = true;
+    useHyperLinks = true;
+  };
+  FixYoutubeEmbeds.enabled = true;
+  ForceOwnerCrown.enabled = true;
+  FriendsSince.enabled = true;
+  ImageZoom = {
+    enabled = true;
+    invertScroll = true;
+    nearestNeighbour = false;
+    saveZoomValues = true;
+    size = 340;
+    square = false;
+    zoom = 4;
+    zoomSpeed = 0.5;
+  };
+  MessageLatency = {
+    detectDiscordKotlin = true;
+    enabled = true;
+    latency = 1;
+    showMillis = true;
+  };
+  NoPendingCount = {
+    enabled = true;
+    hideFriendRequestsCount = false;
+    hideMessageRequestsCount = false;
+    hidePremiumOffersCount = true;
+  };
+  PlatformIndicators = {
+    badges = true;
+    colorMobileIndicator = true;
+    enabled = true;
+    list = true;
+    messages = true;
+  };
+
+  PronounDB = {
+    enabled = true;
+    pronounSource = 0;
+    pronounsFormat = "LOWERCASE";
+    showInMessages = true;
+    showInProfile = true;
+    showSelf = true;
+  };
+
+  RelationshipNotifier = {
+    enabled = true;
+    friendRequestCancels = true;
+    friends = true;
+    groups = true;
+    notices = false;
+    offlineRemovals = true;
+    servers = true;
+  };
+
+  ReviewDB = {
+    enabled = true;
+    hideBlockedUsers = true;
+    hideTimestamps = false;
+    notifyReviews = true;
+    reviewsDropdownState = true;
+    showWarning = true;
+  };
+
+  SendTimestamps = {
+    enabled = true;
+    replaceMessageContents = true;
+  };
+  ServerInfo.enabled = true;
+
+  ShikiCodeblocks = {
+    bgOpacity = 100;
+    enabled = true;
+    theme = "https://raw.githubusercontent.com/shikijs/shiki/0b28ad8ccfbf2615f2d9d38ea8255416b8ac3043/packages/shiki/themes/dark-plus.json";
+    tryHljs = "SECONDARY";
+    useDevIcon = "GREYSCALE";
+  };
+
+  ShowHiddenChannels = {
+    enabled = true;
+    hideUnreads = true;
+    showMode = 0;
+  };
+  TypingIndicator = {
+    enabled = true;
+    includeBlockedUsers = false;
+    includeCurrentChannel = true;
+    includeMutedChannels = false;
+    indicatorMode = 3;
+  };
+  TypingTweaks = {
+    alternativeFormatting = true;
+    enabled = true;
+    showAvatars = true;
+    showRoleColors = true;
+  };
+  USRBG = {
+    enabled = true;
+    nitroFirst = true;
+    voiceBackground = true;
+  };
+  UserVoiceShow = {
+    enabled = true;
+    showInUserProfileModal = true;
+    showVoiceChannelSectionHeader = true;
+  };
+  VoiceDownload.enabled = true;
+  VoiceMessages = {
+    echoCancellation = true;
+    enabled = true;
+    noiseSuppression = true;
+  };
+  WebKeybinds.enabled = true;
+  YoutubeAdblock.enabled = true;
+  oneko.enabled = true;
+  petpet.enabled = true;
+}
