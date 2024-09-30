c4f8d68777cc1abfcbd2ec5e8e970a236e6925cb
Thunder <thunder@disroot.org>
2024-09-30 14:09:15 +0300

    lix update

--- flake.nix
+++ flake.nix
@@ -135,7 +135,7 @@
     disko.inputs.nixpkgs.follows = "nixpkgs";
 
     lix-module = {
-      url = "https://git.lix.systems/lix-project/nixos-module/archive/2.90.0-rc1.tar.gz";
+      url = "https://git.lix.systems/lix-project/nixos-module/archive/2.91.0.tar.gz";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
