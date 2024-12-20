4dcaf4b4b59dc4f69186523ba080809347d9c07c
Thunder <thunder@disroot.org>
2024-12-21 00:50:19 +0200

    outdated container system shit

--- hosts/server/new-torrent/default.nix
+++ hosts/server/new-torrent/default.nix
@@ -1,7 +1,19 @@
 {...}: {
+  imports = [
+    ./virtualhosts.nix
+  ];
+
   meow.sops.enableSecrets = [
     "server_airvpn_private"
     "server_airvpn_preshared"
+    "server_slskd_env"
+  ];
+
+  meow.impermanence.ensureDirectories = [
+    "/persist/new_torrent_stack/downloads/incomplete/torrents"
+    "/persist/new_torrent_stack/downloads/incomplete/soulseek"
+    "/persist/new_torrent_stack/downloads/torrents"
+    "/persist/new_torrent_stack/downloads/soulseek"
   ];
 
   networking.nat = {
@@ -16,6 +28,13 @@
     "net.ipv6.conf.all.forwarding" = 1;
   };
 
+  networking.firewall.interfaces."ve-+".allowedTCPPortRanges = [
+    {
+      from = 0;
+      to = 65535;
+    }
+  ];
+
   # meow.sops.secrets."server_airvpn_private".path = "/run/vpncontainer/private_key";
   # meow.sops.secrets."server_airvpn_preshared".path = "/run/vpncontainer/preshared_key";
 
@@ -24,24 +43,27 @@
     privateNetwork = true;
     enableTun = true;
 
-    hostAddress = "192.168.10.1";
-    localAddress = "192.168.10.2";
+    hostAddress = "10.10.10.1";
+    localAddress = "10.10.10.2";
     hostAddress6 = "fc00::1";
     localAddress6 = "fc00::2";
-
-    forwardPorts = [
-      {
-        protocol = "tcp";
-        hostPort = 8080;
-        containerPort = 8080;
-      }
-    ];
 
     ephemeral = true;
     bindMounts = {
       "secrets" = {
+        isReadOnly = true;
         mountPoint = "/run/secrets";
         hostPath = "/run/secrets";
+      };
+      "media" = {
+        isReadOnly = false;
+        mountPoint = "/media";
+        hostPath = "/persist/media";
+      };
+      "downloads" = {
+        isReadOnly = false;
+        mountPoint = "/downloads";
+        hostPath = "/persist/new_torrent_stack/downloads";
       };
     };
 
@@ -51,6 +73,11 @@
       lib,
       ...
     }: {
+      imports = [
+        ./slskd.nix
+        ./torrent.nix
+      ];
+
       system.stateVersion = "24.11";
 
       environment.systemPackages = [pkgs.curl];
@@ -58,25 +85,39 @@
       services.resolved.enable = true;
 
       networking = {
-        defaultGateway.address = "192.168.10.1";
+        defaultGateway.address = "10.10.10.1";
         nameservers = ["1.1.1.1"];
         firewall.checkReversePath = false;
+
+        firewall.interfaces."eth0".allowedTCPPortRanges = [
+          {
+            from = 0;
+            to = 65535;
+          }
+        ];
+
+        firewall.interfaces."eth0".allowedUDPPortRanges = [
+          {
+            from = 0;
+            to = 65535;
+          }
+        ];
 
         useHostResolvConf = lib.mkForce false;
 
         interfaces."eth0".ipv4.routes = [
           {
-            address = "192.168.10.0";
+            address = "10.10.10.0";
             prefixLength = 24;
-            via = "192.168.10.1";
+            via = "10.10.10.1";
           }
         ];
       };
 
       systemd.services."wg-quick-airvpn-sweden" = {
         # path = [pkgs.bind.host];
-        # preStart = "until host se3.vpn.airdns.org; do sleep 1; done";
-        requires = ["systemd-resolved.service"];
+        # preStart = "until host google.com; do sleep 1; done";
+        # requires = ["systemd-resolved.service"];
 
         serviceConfig = {
           Restart = lib.mkForce "on-failure";
@@ -92,7 +133,23 @@
         privateKeyFile = "/run/secrets/server_airvpn_private";
         mtu = 1320;
 
-        postUp = "${pkgs.iptables}/bin/iptables -I OUTPUT ! -o airvpn-sweden -m mark ! --mark $(wg show airvpn-sweden fwmark) -m addrtype ! --dst-type LOCAL -j REJECT";
+        # postUp = "${pkgs.iptables}/bin/iptables -I OUTPUT ! -o airvpn-sweden -m mark ! --mark $(wg show airvpn-sweden fwmark) -m addrtype ! --dst-type LOCAL -j REJECT";
+
+        # postUp = ''
+        #   "${pkgs.iptables}/bin/iptables -I OUTPUT -o eth0 -j DROP"
+        #   "${pkgs.iptables}/bin/iptables -I INPUT -i eth0 -j DROP"
+
+        #   "${pkgs.iptables}/bin/iptables -I OUTPUT -o eth0 -d 10.10.10.1 -j ACCEPT"
+        #   "${pkgs.iptables}/bin/iptables -I INPUT -i eth0 -d 10.10.10.1 -j ACCEPT"
+        # '';
+
+        # postUp = ''
+        #   ${pkgs.iptables}/bin/iptables -I INPUT ! -i airvpn-sweden -m mark ! --mark $(wg show airvpn-sweden fwmark) -j DROP
+        #   ${pkgs.iptables}/bin/iptables -I OUTPUT ! -o airvpn-sweden -m mark ! --mark $(wg show airvpn-sweden fwmark) -j DROP
+
+        #   ${pkgs.iptables}/bin/iptables -I OUTPUT ! -o airvpn-sweden -m mark ! --mark $(wg show airvpn-sweden fwmark) -d 10.10.10.1 -j ACCEPT
+        #   ${pkgs.iptables}/bin/iptables -I INPUT ! -i airvpn-sweden -m mark ! --mark $(wg show airvpn-sweden fwmark) -d 10.10.10.1 -j ACCEPT
+        # '';
         # There is no preDown on purpose, just restart the container if the vpn ever goes down
 
         peers = [
