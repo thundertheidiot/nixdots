0b59a925205eb61351e17b9ad2fb25cb8dda9714
Thunder <thunder@disroot.org>
2025-01-18 14:32:33 +0200

    remove the old .system attribute

--- hosts/server/default.nix
+++ hosts/server/default.nix
@@ -1,85 +1,83 @@
 {
-  system = {
-    config,
-    lib,
-    mlib,
-    ...
-  }: let
-    inherit (mlib) mkOpt;
-    inherit (lib.types) str;
+  config,
+  lib,
+  mlib,
+  ...
+}: let
+  inherit (mlib) mkOpt;
+  inherit (lib.types) str;
 
-    cfg = config.server;
-  in {
-    options = {
-      # here for easier changing in case of router change etc.
-      server.addr = mkOpt str "192.168.101.101" {};
+  cfg = config.server;
+in {
+  options = {
+    # here for easier changing in case of router change etc.
+    server.addr = mkOpt str "192.168.101.101" {};
+  };
+
+  imports = [
+    ./disko.nix
+    ./dns.nix
+    ./docker
+    ./homepage.nix
+    ./jellyfin.nix
+    ./rathole.nix
+    ./vaultwarden.nix
+    ./webserver.nix
+  ];
+
+  config = {
+    system.stateVersion = "24.11";
+    time.timeZone = "Europe/Helsinki";
+    networking.hostName = "uwu";
+
+    nix.gc = {
+      automatic = true;
+      dates = "daily";
+      options = "--delete-older-than 7d";
     };
 
-    imports = [
-      ./disko.nix
-      ./dns.nix
-      ./docker
-      ./homepage.nix
-      ./jellyfin.nix
-      ./rathole.nix
-      ./vaultwarden.nix
-      ./webserver.nix
-    ];
+    # networking.firewall.allowedTCPPorts = [
+    #   6443
+    # ];
+    # networking.firewall.allowedUDPPorts = [
+    # ];
+    # services.k3s = {
+    #   enable = true;
+    #   role = "server";
+    #   extraFlags = [
+    #     "--disable=traefik"
+    #   ];
+    # };
 
-    config = {
-      system.stateVersion = "24.11";
-      time.timeZone = "Europe/Helsinki";
-      networking.hostName = "uwu";
+    meow = {
+      workstation.enable = false;
+      shell.enable = true;
 
-      nix.gc = {
-        automatic = true;
-        dates = "daily";
-        options = "--delete-older-than 7d";
-      };
+      ssh.key = false; # no user to give to
+      ssh.rootKey = true;
 
-      # networking.firewall.allowedTCPPorts = [
-      #   6443
-      # ];
-      # networking.firewall.allowedUDPPorts = [
-      # ];
-      # services.k3s = {
-      #   enable = true;
-      #   role = "server";
-      #   extraFlags = [
-      #     "--disable=traefik"
-      #   ];
-      # };
+      gpu = "intel";
 
-      meow = {
-        workstation.enable = false;
-        shell.enable = true;
+      impermanence.enable = true;
+      impermanence.persist = "/nix/persist";
+      impermanence.directories = [
+        {
+          path = "/var/lib/rancher";
+        }
+      ];
 
-        ssh.key = false; # no user to give to
-        ssh.rootKey = true;
-
-        gpu = "intel";
-
-        impermanence.enable = true;
-        impermanence.persist = "/nix/persist";
-        impermanence.directories = [
-          {
-            path = "/var/lib/rancher";
-          }
-        ];
-
-        home.enable = false;
-      };
-
-      boot.initrd.availableKernelModules = ["xhci_pci" "ehci_pci" "ahci" "usbhid" "usb_storage" "sd_mod"];
-      boot.initrd.kernelModules = [];
-      boot.kernelModules = ["kvm-intel"];
-      boot.extraModulePackages = [];
-      boot.kernel.sysctl = {
-        "fs.inotify.max_user_watches" = "1048576";
-      };
-
-      nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
-      hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+      home.enable = false;
     };
+
+    boot.initrd.availableKernelModules = ["xhci_pci" "ehci_pci" "ahci" "usbhid" "usb_storage" "sd_mod"];
+    boot.initrd.kernelModules = [];
+    boot.kernelModules = ["kvm-intel"];
+    boot.extraModulePackages = [];
+    boot.kernel.sysctl = {
+      "fs.inotify.max_user_watches" = "1048576";
+    };
+
+    nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+    hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
   };
 }
