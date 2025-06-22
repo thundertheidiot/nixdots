c3473e0d85f3d0c7ccdfcb68a752fb0898dfed09
Thunder <thunder@disroot.org>
2025-06-22 12:54:06 +0300

    sponsorblock extension

--- home/browser/extensions.nix
+++ home/browser/extensions.nix
@@ -1,6 +1,8 @@
 {
   # ublock origin
   "uBlock0@raymondhill.net" = "https://addons.mozilla.org/firefox/downloads/latest/ublock-origin/addon-11423598-latest.xpi";
+  # sponsorblock
+  "sponsorBlocker@ajay.app" = "https://addons.mozilla.org/firefox/downloads/latest/sponsorblock/addon-13499177-latest.xpi";
   # i still don't care about cookies
   "idcac-pub@guus.ninja" = "https://addons.mozilla.org/firefox/downloads/latest/istilldontcareaboutcookies/addon-17568914-latest.xpi";
   # bitwarden
