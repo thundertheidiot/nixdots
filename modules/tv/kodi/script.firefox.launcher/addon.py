0f9e637feecdcbcef00f73ad0312e419d19dac5b
Thunder <thunder@disroot.org>
2025-06-23 17:31:24 +0300

    firefox script new tv kodi

---
+++ modules/tv/kodi/script.firefox.launcher/addon.py
@@ -0,0 +1,29 @@
+import xbmcaddon
+import xbmcgui
+import xbmc
+import os
+import subprocess
+import json
+ 
+addon       = xbmcaddon.Addon()
+addonname   = addon.getAddonInfo('name')
+
+home = os.path.expanduser("~")
+json_path = os.path.join(home, 'kodi_launcher.json')
+
+try:
+    with open(json_path, 'r') as f:
+        program_data = json.load(f)
+except Exception as e:
+    xbmcgui.Dialog().ok("Launcher error", f"Could not load {json_path}:\n{e}")
+    raise SystemExit
+
+names = list(program_data.keys())
+
+index = xbmcgui.Dialog().select("Launch:", names)
+
+if index != -1:
+    selected = names[index]
+    executable = program_data[selected]
+    subprocess.run([executable])
+
