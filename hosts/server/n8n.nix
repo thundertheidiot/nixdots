12b845ef925ce9174b30dcc7c0c5048a16795e86
Thunder <thunder@disroot.org>
2025-07-28 20:49:38 +0300

    experimental n8n on server

---
+++ hosts/server/n8n.nix
@@ -0,0 +1,46 @@
+{
+  config,
+  lib,
+  inputs,
+  ...
+}: {
+  config = {
+    server.domains = [
+      "n8n.home"
+    ];
+
+    meow.impermanence.directories = [
+      {
+        path = "/var/lib/private/n8n";
+      }
+    ];
+
+    services.nginx.virtualHosts."n8n.home" = let
+      certs = import "${inputs.self.outPath}/certs";
+    in {
+      forceSSL = lib.mkForce true;
+
+      sslCertificate = certs."local.crt";
+      sslCertificateKey = config.sops.secrets.localKey.path;
+
+      locations = {
+        "/" = {
+          proxyPass = "http://127.0.0.1:${toString config.services.n8n.settings.port}";
+          recommendedProxySettings = true;
+          extraConfig = ''
+            proxy_set_header Upgrade $http_upgrade;
+            proxy_set_header Connection "Upgrade";
+          '';
+        };
+      };
+    };
+
+    services.n8n = {
+      enable = true;
+      openFirewall = true;
+      settings = {
+        N8N_SECURE_COOKIE = false;
+      };
+    };
+  };
+}
