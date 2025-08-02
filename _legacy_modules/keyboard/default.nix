1bc10618bc85fe4f2fba36e231314352661fc379
Thunder <thunder@disroot.org>
2025-08-02 22:08:16 +0300

    hopefully kanata symbol layer

--- _legacy_modules/keyboard/default.nix
+++ _legacy_modules/keyboard/default.nix
@@ -50,26 +50,29 @@ in {
             ;;      2000
             ;;      (1 2 3 4 5 6 7 8 9 0 - = q w e r t y u i o p [ ] \ a s d f g h j k l ' z x c v b n m , . / )
             ;;      (bspc))
-            esc (tap-hold-release 175 175 esc lmet)
-            spc (tap-hold-release 175 175 spc (layer-while-held numpad))
+            esc (tap-hold-release 150 150 esc lmet)
+            spc (tap-hold-release 150 150 spc (layer-while-held numpad))
+
+            z (tap-hold-release 150 150 z (layer-while-held symbol))
+            / (tap-hold-release 150 150 / (layer-while-held symbol))
 
             ;; home row mods
-            a (tap-hold-release 175 175 a lmet)
-            s (tap-hold-release 175 175 s lalt)
-            d (tap-hold-release 175 175 d lsft)
-            f (tap-hold-release 175 175 f lctl)
+            a (tap-hold-release 150 150 a lmet)
+            s (tap-hold-release 150 150 s lalt)
+            d (tap-hold-release 150 150 d lsft)
+            f (tap-hold-release 150 150 f lctl)
 
-            j (tap-hold-release 175 175 j lctl)
-            k (tap-hold-release 175 175 k rsft)
-            l (tap-hold-release 175 175 l ralt)
-            ; (tap-hold-release 175 175 ; rmet))
+            j (tap-hold-release 150 150 j lctl)
+            k (tap-hold-release 150 150 k rsft)
+            l (tap-hold-release 150 150 l ralt)
+            ; (tap-hold-release 150 150 ; rmet))
 
           (deflayer base
             esc
             grv  1    2    3    4    5    6    7    8    9    0    -     =    bspc
             tab  q    w    e    r    t    y    u    i    o    p    [     ]    ret
             @esc @a   @s   @d   @f   g    h    @j   @k   @l   @;   '     \
-            lsft 102d z    x    c    v    b    n    m    ,    .    /     rsft
+            lsft 102d @z    x    c    v    b    n    m    ,    .    @/     rsft
             lctl lmet lalt           @spc           @raise @base up
                                                               left down rght
           )
@@ -89,8 +92,18 @@ in {
             _    _    _    _    _    _    _    _    _    _    _    _     _     _
             _    _    _    _    _    _    _    7    8    9    _    _     _     _
             _    lmet lalt lsft lctl _    _    4    5    6    _    _     _
-            _    _    _    _    _    _    _    _    1    2    3    _     _
+            _    _    _    _    _    _    _    0    1    2    3    _     _
             _    _    _              _              0    _    _
+                                                              _    _    _
+          )
+
+          (deflayer symbol
+            _
+            _    _    _    _    _    _    _    _    _    _    _    _     _     _
+            _    `    <    >    -    |    ^    {    }    $    _    _     _     _
+            _    !    *    /    =    &    #    (    )    ;    "    _     _
+            _    ~    +    [    ]    %    @    :    ,    .    '    _     _
+            _    _    _              _              _    _    _
                                                               _    _    _
           )
 
