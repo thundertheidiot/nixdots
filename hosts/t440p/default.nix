5176008157e6d693029624228cb8883118284f90
Thunder <thunder@disroot.org>
2025-09-01 10:15:42 +0300

    cert

--- hosts/t440p/default.nix
+++ hosts/t440p/default.nix
@@ -22,6 +22,32 @@
 
   users.users.thunder.initialPassword = "password";
 
+  security.pki.certificates = [
+    ''
+      -----BEGIN CERTIFICATE-----
+      MIIDdzCCAl+gAwIBAgIQZqyAKU9rZpVPfh7Yfh2LxTANBgkqhkiG9w0BAQUFADBC
+      MRIwEAYKCZImiZPyLGQBGRYCZmkxFzAVBgoJkiaJk/IsZAEZFgdjZW50cmlhMRMw
+      EQYDVQQDEwpjZW50cmlhLUNBMB4XDTE1MTIwODEwMDMwMVoXDTQwMTIwODEwMTMw
+      MFowQjESMBAGCgmSJomT8ixkARkWAmZpMRcwFQYKCZImiZPyLGQBGRYHY2VudHJp
+      YTETMBEGA1UEAxMKY2VudHJpYS1DQTCCASIwDQYJKoZIhvcNAQEBBQADggEPADCC
+      AQoCggEBAJ5oNRf1Q7SMuZOCTqz0NWx/6mL7HinOmAYxWtX0MolZvumGUEVD711g
+      wjmD4n2rgFz6j/afNA6EM7a+tgFDnWNwSxzZkopqu5pkif9/kWu1edqbyHmJRAC3
+      ul8mUQUSdWaAW/oGM8CkGqozg3F/lusUsohoVhpkWwVNEhZSBtu8Z/rissPA7pXA
+      gG9yhpr28CfwW9Rg64Gq2tDQtxr+xVt7lYVJe/dCggtvyBgTn9AFe/NCiocGfqOw
+      47y2Q0M/NpE1tnsFhyzWpspmE0236F3Z2qfDwopQsNgjr0mT1atPJpYKOHfBhT1l
+      GtIDVizqB7v/feryBWXOjAVnFOMgPMECAwEAAaNpMGcwEwYJKwYBBAGCNxQCBAYe
+      BABDAEEwDgYDVR0PAQH/BAQDAgGGMA8GA1UdEwEB/wQFMAMBAf8wHQYDVR0OBBYE
+      FNe8NC6s4083zu5iFogjHtbrEeTfMBAGCSsGAQQBgjcVAQQDAgEAMA0GCSqGSIb3
+      DQEBBQUAA4IBAQArJVsKfE/TEI3qypF/VevKFQmbutGj4IdnziREDg//ROtIltnj
+      21eQScPWIvMlxPhponjohgXYG8MaRL7n02e6ugxVRVlG4J2BtmBsa+mdjceWcGwj
+      J3n3RuxDAjIyxbwhJ/gC3JaBBKJ9AEcgcwhlf7fa7+4KAVfjhHmNS+ACrGmm2kef
+      6NHlou4vDw7toCmOJ1s6AJhy7z8mp54NOm0FJEeNXdEMvujcTHdMTixXjbse6FWK
+      PjT5a0LdlUY2ZdYbskECp4nPm83LeKppPv13Quq/YZAtHvFXwDCDgEN3bMFEaMwY
+      Wlyku1axVbOOXygANEc0ueAZZHXzyERKB+B6
+      -----END CERTIFICATE-----
+    ''
+  ];
+
   home-manager.sharedModules = [
     {
       home.stateVersion = "25.05";
