0301d7fb4471de40dd76ca5844161e4222eac35e
Thunder <thunder@disroot.org>
2025-09-14 00:23:17 +0300

    reverse proxy helper

--- modules/server/webserver.nix
+++ modules/server/webserver.nix
@@ -4,19 +4,32 @@
   lib,
   ...
 }: let
-  inherit (mlib) mkEnOpt;
-  inherit (lib) mkIf;
+  inherit (mlib) mkEnOpt mkOpt;
+  inherit (lib) mkIf attrNames mapAttrs;
+  inherit (lib.types) attrsOf str;
 
   cfg = config.meow.server;
 in {
   options.meow.server.webserver = mkEnOpt "Nginx";
+  options.meow.server.reverseProxy = mkOpt (attrsOf str) {} {};
 
   config = mkIf cfg.webserver {
     networking.firewall.allowedTCPPorts = [80 443];
 
+    meow.server.certificates = attrNames cfg.reverseProxy;
+
     services.nginx = {
       enable = true;
       logError = "stderr debug";
+
+      virtualHosts =
+        mapAttrs (_: proxyPass: {
+          locations."/" = {
+            inherit proxyPass;
+            recommendedProxySettings = true;
+          };
+        })
+        cfg.reverseProxy;
     };
   };
 }
