5aebbe8e6787f6368dfa5cdc37cfda61f5759efb
Thunder <thunder@disroot.org>
2025-10-15 11:50:40 +0300

    update certificates for home server

--- certs/default.nix
+++ certs/default.nix
@@ -14,6 +14,23 @@
     security.pki.certificateFiles = [./rootCA.pem];
   };
 
+  domains = [
+    "auth.home"
+    "n8n.home"
+    "git.home"
+    "jellyfin.home"
+    "reddit.home"
+    "firefox.home"
+    "torrent.home"
+    "radarr.home"
+    "sonarr.home"
+    "prowlarr.home"
+    "lidarr.home"
+    "bazarr.home"
+    "immich.home"
+    "homeassistant.home"
+  ];
+
   "local.crt" = ./local.crt;
   "local.csr" = ./local.csr;
   "rootCA.pem" = ./rootCA.pem;
