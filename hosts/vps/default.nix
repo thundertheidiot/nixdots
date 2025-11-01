f4fc3315835593774f49b78ce44a54ef0cb52fc6
Thunder <thunder@disroot.org>
2025-11-01 20:44:14 +0200

    ignore

--- hosts/vps/default.nix
+++ hosts/vps/default.nix
@@ -46,8 +46,7 @@ in {
 
       openssh.authorizedKeys.keys = [
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBKwHM/9spQfyeNIl/p8N8XBuoKj8UrhuhhlbEwkrgjZ thunder@disroot.org"
-        "ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBNt6WrBoFe4rqNeQPU0limd8ROaR2Q+1O5tUB4k5S3oRt8JPgA/9+3UHwMh5lhDsxLnlElASNtwEwBpnYV/0MW8=
-" # orgzly
+        "ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBE6bPM6n9EHBHhkFe9KZ6QrHnCtdCFWSZQp6OLDNlIhUz4YB1ZYMIB1E0iEyW0XiICOeWYcy/MoYCAnJafF+Xhs=" # orgzly
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOMKvFyn0+PoSl4PWZPyg2A+feiC3wgYElw+p2MPLTmy u0_a298@localhost" # termux
       ];
     };
