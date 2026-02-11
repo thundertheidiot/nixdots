c6a55ad968f114bfbe599c1a10158ca13d566be4
Thunder <thunder@disroot.org>
2026-02-11 13:39:55 +0200

    meowdzbot health check

--- hosts/vps/gatus.nix
+++ hosts/vps/gatus.nix
@@ -31,6 +31,15 @@
               "[STATUS] == 200"
             ];
           }
+          {
+            name = "MeowDZBot";
+            group = "VPS";
+            url = "http://127.0.0.1:8080/health";
+            interval = "30s";
+            conditions = [
+              "[STATUS] == 200"
+            ];
+          }
         ];
       };
     };
