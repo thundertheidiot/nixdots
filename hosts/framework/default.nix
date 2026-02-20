385a762257096a792a851d08c9d204c5da4f096a
Thunder <thunder@disroot.org>
2026-02-20 15:10:58 +0200

    framework power management tweaks

--- hosts/framework/default.nix
+++ hosts/framework/default.nix
@@ -11,6 +11,7 @@
     ./disko.nix
     inputs.nixos-hardware.nixosModules.framework-13-7040-amd
     ./kmod.nix # nixos-hardware workaround
+    inputs.autoaspm.nixosModules.default
   ];
 
   system.stateVersion = "25.05";
@@ -29,30 +30,15 @@
   powerManagement.enable = true;
   powerManagement.powertop.enable = true;
 
+  services.power-profiles-daemon.enable = true;
+  boot.kernelParams = ["amd_pstate=active"];
+  services.autoaspm.enable = true;
+
   services.fprintd = {
     enable = true;
   };
 
   boot.kernelPackages = pkgs.cachyosKernels.linuxPackages-cachyos-latest-lto-zen4;
-
-  services.power-profiles-daemon.enable = lib.mkForce false;
-  services.tlp = {
-    enable = true;
-    settings = {
-      CPU_SCALING_GOVERNOR_ON_AC = "performance";
-      CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
-
-      CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
-      CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
-
-      CPU_MIN_PERF_ON_AC = 0;
-      CPU_MAX_PERF_ON_AC = 100;
-      CPU_MIN_PERF_ON_BAT = 0;
-      CPU_MAX_PERF_ON_BAT = 100;
-
-      STOP_CHARGE_THRESH_BAT0 = 90;
-    };
-  };
 
   virtualisation.docker.enable = true;
 
