bf0d3908f42687f9dac077585a0e2e77de6a6214
Thunder <thunder@disroot.org>
2026-03-15 13:48:57 +0200

    meow helper nix build

--- meow/Main.hs
+++ meow/Main.hs
@@ -2,6 +2,7 @@
 
 module Main where
 
+import Distribution.Simple.Utils
 import System.Environment
 
 import Wireguard
@@ -18,8 +19,11 @@ main = do
     emacs <- inEmacs
     args <- getArgs
 
-    putStrLn $ show args
+    case safeHead args of
+        Just "wg" -> do
+            showKeys "sops/wireguard"
+        _ -> putStrLn "ligma"
 
-    showKeys "../sops/wireguard"
+    showKeys "sops/wireguard"
 
     putStrLn $ "Emacs: " ++ show emacs
