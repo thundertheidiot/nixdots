4595e9157848189ec85ecb6295b4c1586d931e54
Thunder <thunder@disroot.org>
2026-03-18 20:46:45 +0200

    sable (cinny fork) instead of cinny

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -2,6 +2,7 @@
 with pkgs; {
   cru = callPackage ./cru.nix {};
   dgr = callPackage ./dgr.nix {};
+  sable = callPackage ./sable.nix {};
   kodi = callPackage ./kodi {};
   screenshot = callPackage ./screenshot {};
   wl_screenshot = callPackage ./wl_screenshot.nix {};
---
+++ pkgs/sable.nix
@@ -0,0 +1,42 @@
+{
+  fetchFromGitHub,
+  fetchPnpmDeps,
+  nodejs,
+  pnpmConfigHook,
+  pnpm_10,
+  git,
+  stdenv,
+}:
+stdenv.mkDerivation (finalAttrs: {
+  pname = "sable";
+  version = "1.9.3";
+
+  src = fetchFromGitHub {
+    owner = "SableClient";
+    repo = "Sable";
+    rev = "v${finalAttrs.version}";
+    hash = "sha256-MMwqQFEW3giu205YqYpPqtQHBPwsrVfn2gOeA3136VA=";
+  };
+
+  pnpmDeps = fetchPnpmDeps {
+    inherit (finalAttrs) pname version src;
+    pnpm = pnpm_10;
+    fetcherVersion = 3;
+    hash = "sha256-3qlawvy4/+W82Ia9oNDKHiqOw4lEujcrMOVaV5PY0dg=";
+  };
+
+  nativeBuildInputs = [
+    nodejs
+    pnpmConfigHook
+    pnpm_10
+    git
+  ];
+
+  buildPhase = ''
+    pnpm run build
+  '';
+
+  installPhase = ''
+    cp -r dist $out
+  '';
+})
