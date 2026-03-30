6a4b15edbf56c41114a9f52501e8308fd4cb8831
Thunder <thunder@disroot.org>
2026-03-30 10:49:09 +0300

    sable bump

--- pkgs/sable.nix
+++ pkgs/sable.nix
@@ -9,20 +9,20 @@
 }:
 stdenv.mkDerivation (finalAttrs: {
   pname = "sable";
-  version = "1.13.0";
+  version = "1.13.1";
 
   src = fetchFromGitHub {
     owner = "SableClient";
     repo = "Sable";
     rev = "v${finalAttrs.version}";
-    hash = "sha256-PggtBXvCX7KFZPqSwPPd9UEjjkBAZZucr6Nmn0DUHuo=";
+    hash = "sha256-1W7kcVpmSQ0y057CKouzEeOhcptWxBjkNDfe53U+3g8=";
   };
 
   pnpmDeps = fetchPnpmDeps {
     inherit (finalAttrs) pname version src;
     pnpm = pnpm_10;
     fetcherVersion = 3;
-    hash = "sha256-DO8/wCnQY5Wxr4qRiflMKbCKR16Hy7jswQouN3xMYXw=";
+    hash = "sha256-bWTjXJ1XJuJflqmJACZwpKZwxctdK1C174Nhym9nYRI=";
   };
 
   nativeBuildInputs = [
