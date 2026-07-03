27bb0fcbbec85da1d31de97b48b14aa40af446e4
Thunder <thunder@disroot.org>
2026-07-04 02:02:38 +0300

    emacs bump + fix lsp completion

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,5 +1,5 @@
-((nil . ((eglot-workspace-configuration .
-					(:nixd (:options (:nixos (:expr
-								  "(builtins.getFlake (builtins.toString ./.)).nixosConfigurations.desktop.options"))
-							 (:home-manager (:expr
-									 "(builtins.getFlake (builtins.ToString ./.)).nixosConfigurations.desktop.options.home-manager.users.type.getSubOptions []"))))))))
+;;; Directory Local Variables            -*- no-byte-compile: t -*-
+;;; For more information see (info "(emacs) Directory Variables")
+
+((nix-mode . ((lsp-nix-nixd-home-manager-options-expr . "(builtins.getFlake (builtins.toString ./.)).nixosConfigurations.desktop.options.home-manager.users.type.getSubOptions []")
+			  (lsp-nix-nixd-nixos-options-expr . "(builtins.getFlake (builtins.toString ./.)).nixosConfigurations.desktop.options"))))
